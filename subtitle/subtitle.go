e6d315f1732297f87a85ac675188230e56cbd363
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2023-10-21 19:12:04 +0330

    subtitle : Extra words , make words map

--- subtitle/subtitle.go
+++ subtitle/subtitle.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -24,5 +25,40 @@ func Process(filename string) {
 	bytes, _ := os.ReadFile(path)
 	content := string(bytes)
 	lines := strings.Split(content, "\n")
+	words := ExtractWords(lines)
+	wordsMap := MakeWordsMap(words)
+	fmt.Println(wordsMap)
+}
 
+func ExtractWords(lines []string) []string {
+	words := []string{}
+	indexer := 0
+	for _, line := range lines {
+		if line == "\r" {
+			indexer += 2
+			continue
+		}
+		if indexer > 0 {
+			indexer--
+			continue
+		}
+
+		newWords := strings.Split(line, " ")
+		words = append(words, newWords...)
+	}
+	return words
+}
+
+func MakeWordsMap(words []string) map[string]int {
+	wordsMap := make(map[string]int, 0)
+
+	for _, word := range words {
+		word := strings.NewReplacer(",", "", ".", "", "\r", "", "\"").Replace(word)
+
+		if _, ok := wordsMap[word]; !ok {
+			wordsMap[word] = 0
+		}
+		wordsMap[word]++
+	}
+	return wordsMap
 }
