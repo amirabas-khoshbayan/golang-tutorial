5f7b330ab7c43c6f78290da26444730fa7f1fcb0
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2023-10-22 14:44:45 +0330

    worker : Do the  works with concurrent , Wait Group

--- concurrency/worker/main.go
+++ concurrency/worker/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type worker struct {
 	color string
 }
+
+//A goroutine is a lightweight thread managed by the Go runtime (created in us, around 2.6 KB memory)
+//Go runtime is a part of Go executable that manges our Go program (e.g. memory allocation, channels communication, goroutines creation) and Communicate with OS kernel
+//A Thread is a sequence of independent instructions needs to be processed by CPU
 
 func main() {
 	jobs := []string{"Job 1", "Job 2", "Job 3", "Job 4", "Job 5", "Job 6"}
@@ -16,14 +21,20 @@ func main() {
 	purple := worker{"purple"}
 	orange := worker{"orange"}
 
-	blue.doJobs(jobs[:2])
-	purple.doJobs(jobs[2:4])
-	orange.doJobs(jobs[4:6])
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go blue.doJobs(jobs[:2], &wg)
+	go purple.doJobs(jobs[2:4], &wg)
+	go orange.doJobs(jobs[4:6], &wg)
+	wg.Wait()
+
 }
 
-func (w worker) doJobs(jobs []string) {
+func (w worker) doJobs(jobs []string, wg *sync.WaitGroup) {
 	for _, job := range jobs {
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 1)
 		fmt.Println(job, "by", w.color, "is done")
 	}
+	wg.Done()
 }
