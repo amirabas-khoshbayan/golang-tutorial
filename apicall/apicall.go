ed21a369a246d5fd45b843e21f32912a26ad5bc4
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2023-10-22 12:52:29 +0330

    api call : io read all , new decoder

--- apicall/apicall.go
+++ apicall/apicall.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
 func main() {
 	client := &http.Client{}
-	resp, err := client.Get("https://jsonplaceholder.typicode.com/posts")
+	//resp, err := client.Get("https://jsonplaceholder.typicode.com/posts")
+	resp, err := client.Get("https://dummyjson.com/products")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	defer resp.Body.Close()
-	totalBytes := make([]byte, 0)
-	bs := make([]byte, 4)
-	for err != io.EOF {
-		_, err = resp.Body.Read(bs)
-		totalBytes = append(totalBytes, bs...)
-	}
-	fmt.Println(string(totalBytes))
+	var myMap = map[string]any{}
+
+	json.NewDecoder(resp.Body).Decode(&myMap)
+	fmt.Println(myMap)
+
+	//io.Copy(os.Stdout, resp.Body)
+	//totalBySlice, err := io.ReadAll(resp.Body)
+	//json.Unmarshal(totalBySlice, &myMap)
+
 }
