4b813b6bd5e344ac02cc2f72c05ee333f877ca7c
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2023-10-27 11:03:08 +0330

    concurrency : Memory Share : Mutex

--- concurrency/memoryshare/memory_share.go
+++ concurrency/memoryshare/memory_share.go
@@ -5,20 +5,43 @@ import (
 	"sync"
 )
 
+// go run -race memory_share.go
+// check Mutex for access control Goroutine to Memory
+
+type Counter struct {
+	sync.Mutex
+	Value int
+}
+
 func main() {
 	fmt.Println("Start")
 	var wg sync.WaitGroup
+	counter := Counter{}
 
-	for i := 1; i <= 10; i++ {
+	//for i := 1; i <= 10; i++ {
+	//	wg.Add(1)
+	//	go printMessage(i, &wg)
+	//}
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
-		go printMessage(i, &wg)
+		go incerement(&counter, &wg)
 	}
+
 	wg.Wait()
 
+	fmt.Println("value: ", counter.Value)
 	fmt.Println("End")
 }
-func printMessage(index int, wg *sync.WaitGroup) {
-	fmt.Println(index)
-	wg.Done() // or wg.Add(-1)
 
+func incerement(counter *Counter, wg *sync.WaitGroup) {
+	counter.Lock()
+	counter.Value++
+	wg.Done()
+	counter.Unlock()
 }
+
+//func printMessage(index int, wg *sync.WaitGroup) {
+//	fmt.Println(index)
+//	wg.Done() // or wg.Add(-1)
+//
+//}
