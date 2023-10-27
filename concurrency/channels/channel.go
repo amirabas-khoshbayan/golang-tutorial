06a486a1a4a5d9c01f369293caf095840b52e5c5
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2023-10-27 19:28:10 +0330

    concurrency : close channel

--- concurrency/channels/channel.go
+++ concurrency/channels/channel.go
@@ -6,18 +6,28 @@ import (
 )
 
 func main() {
-	ch := make(chan string)
+	// Buffer Channel
+	ch := make(chan int, 10)
+
+	//UnBuffer Channel
+	//ch := make(chan int)
 
 	go sendMessage(ch)
+	for message := range ch {
+		fmt.Println(message)
+	}
 
-	time.Sleep(time.Second * 2)
-	message := <-ch
-	fmt.Println(message)
+	//time.Sleep(time.Second * 2)
 
 }
 
-func sendMessage(ch chan string) {
+func sendMessage(ch chan int) {
+	for i := 0; i < cap(ch); i++ {
+		time.Sleep(2 * time.Second)
+		ch <- i
+	}
+
+	close(ch)
 	fmt.Println("finish")
-	ch <- "WelCome Amirabbas"
 
 }
